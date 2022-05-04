5897b48822d1ac99e02af663b68c9e5bcce8e40b
Stanisław Dera <stanislawdera@outlook.com>
2022-05-04 20:46:39 +0200

    Add opening set from file

---
+++ utils/importWizard.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+function importWizard() {
+  console.log(chalk.red("This feature isn't available yet"));
+}
+
+export default importWizard;
--- utils/openSet.js
+++ utils/openSet.js
@@ -1,11 +1,16 @@
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import fs from "fs";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { exit } from "process";
+import importWizard from "./importWizard.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const CONTENTS_FILE_NAME = join(__dirname, "..", "data", "contents.json");
+const DATA_DIRECTORY_NAME = join(__dirname, "..", "data");
+const CONTENTS_FILE_NAME = join(DATA_DIRECTORY_NAME, "contents.json");
 
 const checkIfContentsFileExists = () => {
   try {
@@ -26,9 +31,55 @@ const getContents = () => {
   return contents;
 };
 
-const openSet = () => {
+const getSetById = (id) => {
+  const setPath = join(DATA_DIRECTORY_NAME, `${id}.json`);
+
+  try {
+    return JSON.parse(fs.readFileSync(setPath));
+  } catch {
+    console.log(chalk.red("There was an error! We couldn't open this set :("));
+    exit(1);
+  }
+};
+
+const openSet = async () => {
   const contents = getContents();
-  console.log(JSON.stringify(contents));
+
+  let choices = [
+    ...contents.map((element) => {
+      return {
+        name: element.name,
+        value: element.id,
+      };
+    }),
+    {
+      name: "Create a new set (import from tab-separated values)",
+      value: false,
+    },
+  ];
+
+  if (contents.length > 0) {
+    const setIdPrompt = await inquirer.prompt({
+      name: "set",
+      message: "Which set would you like to learn?",
+      type: "list",
+      choices: choices,
+    });
+
+    if (setIdPrompt.set) {
+      const set = getSetById(setIdPrompt.set);
+      console.log(set);
+    } else {
+      importWizard();
+    }
+  } else {
+    console.log(
+      chalk.cyanBright(
+        "It looks like you are using the Flashkon app for the first time. Let's get started by importing the first set. Prepare a file in which terms and definitions are separated by tabs (TSV)."
+      )
+    );
+    importWizard();
+  }
 };
 
 export default openSet;
